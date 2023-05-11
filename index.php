9213a85893db3293ed618c542edb7f1220ae4e3c
diahayuistiqomah <126170779+diahayuistiqomah@users.noreply.github.com>
2023-05-11 12:01:23 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php require_once './src/templates/header.php'; ?>
+<section class="my-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Selamat datang!</h4>
+                        <p class="card-text">Masuk ke halaman <a href="./src/users">users</a>.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php require_once './src/templates/footer.php'; ?>
